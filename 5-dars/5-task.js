e901d58e39070bb08361ec79df895201f7b904e4
AzizShodmonov1805 <kompyuter030518@gmail.com>
2023-06-19 12:57:39 +0500

    qoshildi

--- 5-dars/5-task.js
+++ 5-dars/5-task.js
@@ -286,3 +286,43 @@ for (let i = 0; i <=n; i++) {
 //     A3=A;
 //     console.log(A);
 // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  task polindrom va teskari sonlarni aniqlash
+function polindrom(son) {
+    
+    function teskari(son) {
+        
+        let yangi=0,qoldiq=0;
+        while (son!==0) {
+            qoldiq=son%10;
+            yangi=yangi*10+qoldiq
+            son=(son-qoldiq)/10;
+                   
+        }
+        return yangi;
+    }
+    if (teskari(son)===son){
+        return"polindrom";
+    }
+    else
+    return " not polindrom";
+
+}
+
+
+// ssssssssgit 
+
+console.log(polindrom(123));
+
